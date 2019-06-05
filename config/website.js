2e248bc4ec8c2792b516fa3cff6c72644b5aa142
Kaozidd <arthur_hemingway@outlook.com>
2019-06-05 16:51:43 -0500

    first commit

--- config/website.js
+++ config/website.js
@@ -3,21 +3,21 @@ const tailwind = require('../tailwind')
 module.exports = {
   pathPrefix: '/', // Prefix for all links. If you deploy your site to example.com/portfolio your pathPrefix should be "/portfolio"
 
-  siteTitle: 'Cara - Gatsby Starter Portfolio', // Navigation and Site Title
-  siteTitleAlt: 'Cara', // Alternative Site title for SEO
-  siteTitleShort: 'Cara', // short_name for manifest
-  siteHeadline: 'Creating marvelous art & blazginly fast websites', // Headline for schema.org JSONLD
-  siteUrl: 'https://cara.lekoarts.de', // Domain of your site. No trailing slash!
-  siteLanguage: 'en', // Language Tag on <html> element
-  siteLogo: '/logo.png', // Used for SEO and manifest
-  siteDescription: 'Playful & Colorful One-Page website with Parallax effect',
-  author: 'LekoArts', // Author for schema.org JSONLD
+  siteTitle: 'PsyCode - Los desarrolladores conscientes', // Navigation and Site Title
+  siteTitleAlt: 'PsyCode', // Alternative Site title for SEO
+  siteTitleShort: 'PsyCode', // short_name for manifest
+  siteHeadline: 'Solucionadores de problemas', // Headline for schema.org JSONLD
+  siteUrl: 'http://psycode-surge.sh', // Domain of your site. No trailing slash!
+  siteLanguage: 'es', // Language Tag on <html> element
+  siteLogo: '/icon.png', // Used for SEO and manifest
+  siteDescription: 'Página de una vista colorida y con efecto parallax',
+  author: 'PsyCode', // Author for schema.org JSONLD
 
   // siteFBAppID: '123456789', // Facebook App ID - Optional
-  userTwitter: '@cara', // Twitter Username
-  ogSiteName: 'cara', // Facebook Site Name
-  ogLanguage: 'en_US', // Facebook Language
-  googleAnalyticsID: 'UA-47519312-5',
+  userTwitter: '@kaozidd', // Twitter Username
+  ogSiteName: 'PsyCode', // Facebook Site Name
+  ogLanguage: 'es_MX', // Facebook Language
+  googleAnalyticsID: 'n/a',
 
   // Manifest and Progress color
   themeColor: tailwind.colors.orange,
